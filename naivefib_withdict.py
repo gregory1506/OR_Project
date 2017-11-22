5ba4e291c1bd8b9a1acacceb36b1cd638e43d749
gregory1506 <gjotomcat@gmail.com>
2017-11-22 16:34:47 -0400

    vcnbvn

--- naivefib_withdict.py
+++ naivefib_withdict.py
@@ -1,9 +1,12 @@
 ''' Naive implementation of Fibonacci sequence '''
-
+past = {}
 def fib(N):
     if N < 2:
         return 1
+    if N in past:
+        return past[N]
     else:
-        return fib(N -1) + fib(N - 2)
+        past[N] = fib(N -1) + fib(N - 2)
+    return past[N] 
 
 print("Fibonnaci of {} is {}".format(40,fib(40)))
