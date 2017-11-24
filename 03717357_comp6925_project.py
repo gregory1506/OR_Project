7827550924de099399a9e131779dc58b0f949002
gregory1506 <gjotomcat@gmail.com>
2017-11-24 14:21:59 -0400

    tugf

--- 03717357_comp6925_project.py
+++ 03717357_comp6925_project.py
@@ -1,1 +1,21 @@
 import networkx
+import itertools as it
+
+M = 5
+k = 2
+def digitsum(M,k):
+    l = []
+    precision = k
+    for i in range(pow(10,k)):
+        s = f'{i:0{precision}}'
+        tot = 0
+        for x in s:
+            tot += int(x)
+        if tot == M:
+            l.append(tuple(map(int,tuple(s))))
+    return l
+for outcome in it.product('01',repeat=4):
+    print(tuple(map(int,outcome)))
+for comb in it.combinations('ABCD',3):
+    print(comb)
+print(digitsum(5,2))
